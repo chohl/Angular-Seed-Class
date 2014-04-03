f3d2f12fa65ed26556046b23292644a74b76b346
chohl <chohlclass@gmail.com>
2014-04-03 14:08:30 -0500

    modified files for Class 4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,33 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope', function($scope) {
+		$scope.car = {make: "Honda",
+				model: "Civic",
+				year: 2014,
+				price: 24000
+				}
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', ['$scope', function($scope) {
+		  $scope.cars = [
+				{make: 'Civic',model: "Civic", year: 2014,price: 24000},
+				{make: 'Toyota',model: "Yaris", year: 2014,price: 12500},
+				{make: 'Ford',model: "Fiat", year: 2009,price: 4000},
+				{make: 'Toyota',model: "Camry", year: 2012,price: 18000}
+			
+		];
   }]);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
